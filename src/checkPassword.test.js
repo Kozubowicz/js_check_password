7c5f0283d7c75eb79c42cd28f9aad3d6cb5c7ddb
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-09-28 10:05:09 +0200

    solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -7,13 +7,32 @@ describe(`Function 'checkPassword':`, () => {
     expect(checkPassword).toBeInstanceOf(Function);
   });
 
-  it(`should return boolean`, () => {
+  it(`should return boolean`, () => {});
 
-  });
-
-  it(`should return 'true' for the valid password with 8 characters`, () => {
-
-  });
+  it(`should return 'true' for the valid password with 8 characters`, () => {});
 
   // write more tests here
+
+  it(`should return 'true' for the valid password`, () => {
+    expect(checkPassword('sam1@Zqwer')).toBeTruthy();
+  });
+
+  it(`should return 'true' for the valid password`, () => {
+    expect(checkPassword('jACK1__lol')).toBeTruthy();
+  });
+
+  it(`should return 'false' for password bellow 8 characters`, () => {
+    expect(checkPassword('sam1@Z')).toBeFalsy();
+  });
+
+  it(`should return 'false' for password abowe 16 characters`, () => {
+    expect(checkPassword('sam1@Zqwertyuiopasd')).toBeFalsy();
+  });
+
+  it(`should return 'false' for password not containing at least
+     1 digit, 1 special character, 1 uppercase letter`, () => {
+    expect(checkPassword('sam1#asdfgh')).toBeFalsy();
+    expect(checkPassword('samAs#dfghj')).toBeFalsy();
+    expect(checkPassword('samA123asdf')).toBeFalsy();
+  });
 });
