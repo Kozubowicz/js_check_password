65bd732a3c79e47f4672ad2a762b28e30c603fe2
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2024-09-30 15:15:41 +0200

    update of tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,26 +8,32 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-    expect(checkPassword).toBeTruthy();
+    const check = checkPassword('sam1@Zqwer');
+
+    expect(typeof check).toBe('boolean');
+  });
+
+  it(`should return 'true' for password with exactly  8 characters`, () => {
+    expect(checkPassword('sam1@Zqw')).toBe(true);
   });
 
   it(`should return 'true' for the valid password`, () => {
-    expect(checkPassword('sam1@Zqwer')).toBeTruthy();
-    expect(checkPassword('jACK1__lol')).toBeTruthy();
+    expect(checkPassword('sam1@Zqwer')).toBe(true);
+    expect(checkPassword('jACK1__lol')).toBe(true);
   });
 
   it(`should return 'false' for password bellow 8 characters`, () => {
-    expect(checkPassword('sam1@Z')).toBeFalsy();
+    expect(checkPassword('sam1@Za')).toBe(false);
   });
 
   it(`should return 'false' for password abowe 16 characters`, () => {
-    expect(checkPassword('sam1@Zqwertyuiopasd')).toBeFalsy();
+    expect(checkPassword('sam1@Zqwertyuiopa')).toBe(false);
   });
 
   it(`should return 'false' for password not containing at least
      1 digit, 1 special character, 1 uppercase letter`, () => {
-    expect(checkPassword('sam1#asdfgh')).toBeFalsy();
-    expect(checkPassword('samAs#dfghj')).toBeFalsy();
-    expect(checkPassword('samA123asdf')).toBeFalsy();
+    expect(checkPassword('sam1#asdfgh')).toBe(false);
+    expect(checkPassword('samAs#dfghj')).toBe(false);
+    expect(checkPassword('samA123asdf')).toBe(false);
   });
 });
